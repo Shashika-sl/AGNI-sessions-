c833f8a5204534addd10423c9f7cb22b81f46c62
Shashika-sl <sarathchandrasiri895@gmail.com>
2025-09-13 23:38:56 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -2,26 +2,26 @@ import * as mega from 'megajs';
 import fs from 'fs';
 
 const auth = {
-    email: 'shashikasdddrt@gmail.com',
-    password: 'Shashika@2008',
-    userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
-};
+  email: 'dmritasudarsani@gmail.com',
+  password: 'SHASHIKA@2008',
+  userAgent: 'Mozilla/5.0'
+}
 
 export const upload = (data, name) => {
-    return new Promise((resolve, reject) => {
-        try {
-            const storage = new mega.Storage(auth, () => {
-                data.pipe(storage.upload({name: name, allowUploadBuffering: true}));
-                storage.on("add", (file) => {
-                    file.link((err, url) => {
-                        if (err) throw err;
-                        storage.close();
-                        resolve(url);
-                    });
-                });
-            });
-        } catch (err) {
-            reject(err);
-        }
-    });
+  return new Promise((resolve, reject) => {
+    try {
+      const storage = new mega.Storage(auth, () => {
+        data.pipe(storage.upload({ name: name, allowUploadBuffering: true }));
+        storage.on("add", (file) => {
+          file.link((err, url) => {
+            if (err) throw err;
+            storage.close();
+            resolve(url);
+          });
+        });
+      });
+    } catch (err) {
+      reject(err);
+    }
+  });
 };
