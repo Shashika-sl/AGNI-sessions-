4b8be00298a5f52ef09d6fcf80a4f9afa61774f5
Shashika-sl <sarathchandrasiri895@gmail.com>
2025-09-13 22:56:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-const express = require('express');
-const app = express();
-__path = process.cwd()
-const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 8000;
-let code = require('./pair');
-require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/code', code);
+import express from 'express';
+import bodyParser from 'body-parser';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import codeRouter from './pair.js';
 
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+import 'events';
+import { EventEmitter } from 'events';
+EventEmitter.defaultMaxListeners = 500;
+
+app.use('/code', codeRouter);
+
+app.use('/', async (req, res, next) => {
+    res.sendFile(join(__dirname, 'pair.html'));
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(PORT, () => {
-    console.log(`⏩ Server running on http://localhost:` + PORT)
-})
 
-module.exports = app
+app.listen(PORT, () => {
+    console.log(`⏩ Server running on http://localhost:${PORT}`);
+});
+
+export default app;
